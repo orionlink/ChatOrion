7d6a33d7606f900506545be3845859e46ed0500f
hwk <orionlink@163.com>
2025-01-03 17:18:29 +0800

    server.js

--- ChatOrionServer/protocol/build_proto.sh
+++ ChatOrionServer/protocol/build_proto.sh
@@ -4,8 +4,8 @@
 current_dir=$(pwd)
 
 protofile=message.proto
-protoc_path=$current_dir/../../third/bin/grpc/protoc
-protoc_gen_grpc_path=$current_dir/../../third/bin/grpc/grpc_cpp_plugin
+protoc_path=$current_dir/../../third/linux/grpc/bin/protoc
+protoc_gen_grpc_path=$current_dir/../../third/linux/grpc/bin/grpc_cpp_plugin
 
 $protoc_path -I="." --grpc_out="." --plugin=protoc-gen-grpc="$protoc_gen_grpc_path" "$protofile"
 
@@ -14,7 +14,7 @@ $protoc_path -I="." --cpp_out="." "$protofile"
 # 查找所有 .h, .cpp, .cc, .cxx, .hpp 文件
 files_to_copy=$(find "$current_dir" -maxdepth 1 \( -name "*.h" -or -name "*.cpp" -or -name "*.cc" -or -name "*.cxx" -or -name "*.hpp" \))
 
-# 遍历每个文件并将其拷贝到上级目录
+# 遍历每个文件并将其拷贝到GateServer目录
 for file in $files_to_copy; do
     cp -f "$file" "$current_dir/../GateServer"
     echo "Copied: $file"
