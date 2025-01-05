245bc5adc6d7fff05b50e628e434ce128eaaabbf
hwk <orionlink@163.com>
2025-01-05 21:08:33 +0800

    提交hiredis库

--- ChatOrionServer/GateServer/main.cpp
+++ ChatOrionServer/GateServer/main.cpp
@@ -4,8 +4,14 @@
 #include "cserver.h"
 #include  "Settings.h"
 
+#include <hiredis/hiredis.h>
+
+void TestRedis();
+
 int main()
 {
+    TestRedis();
+
     try
     {
         auto &settings = config::Settings::GetInstance();
@@ -24,6 +30,7 @@ int main()
         std::make_shared<CServer>(ioc, port)->start();
         std::cout << "Gate Server listen on port: " << port << std::endl;
         std::cout << "主线程 id: " << std::this_thread::get_id() << std::endl;
+
         ioc.run();
     }
     catch (std::exception& ex)
@@ -32,4 +39,82 @@ int main()
     }
 
     return 0;
+}
+
+void TestRedis()
+{
+    //连接redis 需要启动才可以进行连接
+//redis默认监听端口为6387 可以再配置文件中修改
+    redisContext* c = redisConnect("127.0.0.1", 6380);
+    if (c->err)
+    {
+        printf("Connect to redisServer faile:%s\n", c->errstr);
+        redisFree(c);        return;
+    }
+    printf("Connect to redisServer Success\n");
+    std::string redis_password = "123456";
+    redisReply* r = (redisReply*)redisCommand(c, "AUTH %s %s", "default", redis_password);
+     if (r->type == REDIS_REPLY_ERROR) {
+         printf("Redis认证失败！错误信息：%s\n", r->str);
+    }else {
+        printf("Redis认证成功！\n");
+         }
+    //为redis设置key
+    const char* command1 = "set stest1 value1";
+    //执行redis命令行
+    r = (redisReply*)redisCommand(c, command1);
+    //如果返回NULL则说明执行失败
+    if (NULL == r)
+    {
+        printf("Execut command1 failure\n");
+        redisFree(c);        return;
+    }
+    //如果执行失败则释放连接
+    if (!(r->type == REDIS_REPLY_STATUS && (strcmp(r->str, "OK") == 0 || strcmp(r->str, "ok") == 0)))
+    {
+        printf("Failed to execute command[%s]\n", command1);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    //执行成功 释放redisCommand执行后返回的redisReply所占用的内存
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command1);
+    const char* command2 = "strlen stest1";
+    r = (redisReply*)redisCommand(c, command2);
+    //如果返回类型不是整形 则释放连接
+    if (r->type != REDIS_REPLY_INTEGER)
+    {
+        printf("Failed to execute command[%s]\n", command2);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    //获取字符串长度
+    int length = r->integer;
+    freeReplyObject(r);
+    printf("The length of 'stest1' is %d.\n", length);
+    printf("Succeed to execute command[%s]\n", command2);
+    //获取redis键值对信息
+    const char* command3 = "get stest1";
+    r = (redisReply*)redisCommand(c, command3);
+    if (r->type != REDIS_REPLY_STRING)
+    {
+        printf("Failed to execute command[%s]\n", command3);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    printf("The value of 'stest1' is %s\n", r->str);
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command3);
+    const char* command4 = "get stest2";
+    r = (redisReply*)redisCommand(c, command4);
+    if (r->type != REDIS_REPLY_NIL)
+    {
+        printf("Failed to execute command[%s]\n", command4);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command4);
+    //释放连接资源
+    redisFree(c);
 }
